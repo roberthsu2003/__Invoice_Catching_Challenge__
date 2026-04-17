85433178402cec38be03b6b21759696ec69d04eb
徐國堂 <roberthsu2003@gmail.com>
2026-04-17 16:23:55 +0800

    改為github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(({mode}) => {
   return {
-    base: './',
+    base: '/__Invoice_Catching_Challenge__-/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
